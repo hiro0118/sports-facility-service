4cab1d479215dce27cd2e203ce9040ca6911fb1a
hiro0118 <fuji.hiroshi0118@gmail.com>
2023-07-04 18:42:45 +0900

    Modify scripts

--- scripts/start-frontend.sh
+++
@@ -1,4 +0,0 @@
-# Start frontend
-script_dir="$(cd "$(dirname "$0")" && pwd)"
-cd $script_dir/../frontend
-npm start
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,15 +3,21 @@
 # Define an array of Docker Compose projects
 apis=("tennis-api")
 
+# Get the directory of this script
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 
 # Start API gateway
 cd $script_dir/../api-gateway
 docker-compose up -d
 
-# Loop through each project and start the Docker Compose
+# Start backend APIs
 for api in "${apis[@]}"; do
   echo "Starting $api..."
   cd $script_dir/../backend/$api
   docker-compose up -d
 done
+
+# Start frontend
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+cd $script_dir/../frontend
+npm start
