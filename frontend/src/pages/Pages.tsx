e2647065128c15dc4122b386e2af2c05a860d39f
hiro0118 <fuji.hiroshi0118@gmail.com>
2024-01-21 22:18:29 +0900

    Create maain page

--- frontend/src/pages/Pages.tsx
+++ frontend/src/pages/Pages.tsx
@@ -1,20 +1,35 @@
 import { TennisCourtsPage } from "./raffle-status-page/RaffleStatusPage";
 import { NotificationConfigPage } from "./notification-config-page/NotificationConfigPage";
 import { PageInfo } from "../components/menu-bar/PageInfo";
-import SportsTennisIcon from '@mui/icons-material/SportsTennis';
+import HomeIcon from '@mui/icons-material/Home';
+import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import { MainPage } from "./main/MainPage";
 
 export const pages: PageInfo[] = [
   {
+    title: "Main",
+    description: "",
+    path: "",
+    icon: HomeIcon,
+    image: "",
+    element: MainPage,
+  },
+  {
     title: "Raffle Status",
+    description: "Raffle Status Description",
     path: "raffle-status",
-    icon: SportsTennisIcon,
+    icon: ConfirmationNumberIcon,
+    image: "/images/raffle.png",
     element: TennisCourtsPage,
+    
   },
   {
     title: "Notification Config",
+    description: "Notification Config Description",
     path: "notification-config",
     icon: NotificationsActiveIcon,
+    image: "/images/notification.png",
     element: NotificationConfigPage,
   }
 ]
